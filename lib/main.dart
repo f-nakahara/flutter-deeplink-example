d6e21a98d28b379ab24ab68dc6a38702767a13a7
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-12-31 03:43:48 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,127 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:uni_links/uni_links.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  StreamSubscription? _sub;
+
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _sub?.cancel();
+    super.dispose();
+  }
+
+  Future<void> init() async {
+    /// DeepLinkを監視する
+    _sub = uriLinkStream.listen((Uri? uri) {
+      if (uri != null) {
+        final host = uri.host; // myapp
+        switch (host) {
+          case "page1": // myapp://page1
+            pushPage(const Page1());
+            break;
+          case "page2": // myapp://page2
+            pushPage(const Page2());
+            break;
+        }
+      }
+    });
+  }
+
+  /// [page]に画面遷移する
+  Future<void> pushPage(Widget page) async {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return page;
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            hyperlinkButton('myapp://page1'), // myapp://page1 を開くボタン
+            hyperlinkButton('myapp://page2'), // myapp://page2 を開くボタン
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget hyperlinkButton(String url) {
+    final uri = Uri.parse(url);
+    return ElevatedButton(
+      onPressed: () async {
+        if (await canLaunchUrl(uri)) {
+          await launchUrl(uri); // URLを開く
+        }
+      },
+      child: Text(url),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Page1'),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Page2'),
+      ),
+    );
+  }
+}
